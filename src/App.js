495c03efcaa5b2ca0ed8bf73cf81c2b49bbb87fa
burak <burak.altuntas@yandex.com>
2021-11-05 20:49:15 +0300

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { DashboardProvider } from "./context/DashboardContext";
+import UserInfo from "./components/UserInfo";
+import CardList from "./components/CardList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+			
+   			
+			<DashboardProvider>
+				<div className="container">
+				<UserInfo />
+				<CardList />
+				</div>
+			</DashboardProvider>
+		
+	);
 }
 
 export default App;
